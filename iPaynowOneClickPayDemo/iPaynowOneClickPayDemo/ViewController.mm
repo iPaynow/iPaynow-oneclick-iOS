90a3abd22fa76f455307da4743f23a41751aa3c3
Hstripe <hrbistu@gmail.com>
2017-04-11 14:49:12 +0800

    add podspec

--- iPaynowOneClickPayDemo/iPaynowOneClickPayDemo/ViewController.mm
+++ iPaynowOneClickPayDemo/iPaynowOneClickPayDemo/ViewController.mm
@@ -41,7 +41,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [IPNOneClickPayPlugin setIPNStemeColor:[UIColor orangeColor]];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
